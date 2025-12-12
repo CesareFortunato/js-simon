2d5cd14e7582cbda2f7dc959a4a5b85d87a99496
CesareFortunato <cesare.fortunato89@gmail.com>
2025-12-12 16:07:43 +0100

    feat: d-none on random numbers + d-none revert on form

--- js/script.js
+++ js/script.js
@@ -2,12 +2,14 @@
 
 // global vars
 
-let timer = 10;
+let timer = 1;
 
 
 // link html elements to js vars
 const countdown = document.getElementById("countdown");
-const randomNumbersList = document.getElementById("numbers-list")
+const randomNumbersList = document.getElementById("numbers-list");
+const instructionText = document.getElementById("instructions");
+const form = document.getElementById("answers-form");
 
 //SYSTEM
 
@@ -21,6 +23,10 @@ const myCountdown = setInterval(() => {
     countdown.innerHTML = timer;
     if (timer === 0) {
         clearInterval(myCountdown);
+        countdown.classList.add('d-none');
+        randomNumbersList.classList.add('d-none');
+        instructionText.classList.add('d-none');
+        form.classList.remove('d-none');
     }
 
 }, 1000)           // countdown till reaches 0

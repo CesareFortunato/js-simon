c573cc124a81404fef022fef1450ee55c1f81556
CesareFortunato <cesare.fortunato89@gmail.com>
2025-12-12 15:23:07 +0100

    feat: setup timer

--- js/script.js
+++ js/script.js
@@ -1,11 +1,25 @@
 // VARIABLES
 
 // link html elements to js vars
-
+const countdown = document.getElementById("countdown");
 
 //SYSTEM
 
-//
+// countdown
+
+let timer = 10;
+countdown.innerHTML = timer;
+
+const myCountdown = setInterval(() => {
+    timer--;
+    console.log(timer);
+     countdown.innerHTML = timer;
+    if(timer === 0){
+        clearInterval(myCountdown);
+    }
+    
+}, 1000)
+
 
 
 

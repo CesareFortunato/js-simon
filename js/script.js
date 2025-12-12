75af576ddc90438d2b76e8bbe7fff043470e2d74
CesareFortunato <cesare.fortunato89@gmail.com>
2025-12-12 15:51:45 +0100

    feat: randomizer function + populate array

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 // VARIABLES
 
+// global vars
+
+let timer = 10;
+
+
 // link html elements to js vars
 const countdown = document.getElementById("countdown");
 
@@ -7,20 +12,41 @@ const countdown = document.getElementById("countdown");
 
 // countdown
 
-let timer = 10;
-countdown.innerHTML = timer;
+
+countdown.innerHTML = timer; // show "10" at the beginning 
 
 const myCountdown = setInterval(() => {
     timer--;
-    console.log(timer);
-     countdown.innerHTML = timer;
-    if(timer === 0){
+    countdown.innerHTML = timer;
+    if (timer === 0) {
         clearInterval(myCountdown);
     }
-    
-}, 1000)
+
+}, 1000)           // countdown till reaches 0
+
+
+// numbers to memorize
+
+console.log(randomNumberArray(1, 50, 5));
 
 
 
 
 // FUNCTIONS
+
+function randomNumberArray(minNum, maxNum, tot){
+    const numArr = [];
+
+    while (numArr.length < tot){
+        const randomNum = genRandomNumber(minNum, maxNum);
+
+        if (!numArr.includes(randomNum)){
+            numArr.push(randomNum)
+        }
+    }
+    return numArr
+}
+
+function genRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
